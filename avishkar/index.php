f51604d613deddfea657f0d9c9e32e09581bf326
Devil5598 <sachinbaiswar@gmail.com>
2018-09-05 00:40:00 +0530

    loginpage

---
+++ avishkar/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>The Lifestyle Store</title>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+        
+        <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
+        
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+    </head>
+    <body>
+       
+        <div class="container-fluid" id="container">
+            
+            <div class="row">
+            <div id="login">
+                    <p>Already have an account? <b>Log In</b></p>
+            </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-lg-4">
+                </div>
+                <div class="col-lg-6" id="facebook">
+                    <img src="facebook.jpg" height="50">
+                </div>
+            </div>
+        </div>
+        
+            
+        
+    </body>
+</html>
---
+++ avishkar/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+         <title>The Music World</title>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+        
+        <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
+        
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+    </head>
+    <body>
+        <div class="box">
+            <h2>Login</h2>
+            
+            <form>
+                <div class="inputbox">
+                    <input type="text" name="" required="">
+                    <label>Username</lable>
+                </div>
+                <div class="inputbox">
+                    <input type="password" name="" required="">
+                    <label>Password</lable>
+                </div>
+                <input type="submit" name="" value="Submit">
+                
+            </form>
+        </div>
+    </body>
+</html>
